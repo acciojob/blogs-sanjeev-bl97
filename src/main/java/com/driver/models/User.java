5c5332aede788f6cc88e4338148ad284ebcc8bf7
sanjeev.bl97 <sanjeev.bl97@gmail.com>
2023-06-16 17:23:45 +0530

    first

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -13,7 +13,7 @@ public class User{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String userName;
+    private String username;
     private String password;
     private String firstName;
     private String lastName ;
@@ -24,16 +24,16 @@ public class User{
     public User() {
     }
 
-    public User(String userName, String password) {
+    public User(String username, String password) {
         this.firstName =  "test";
         this.lastName = "test";
-        this.userName = userName;
+        this.username = username;
         this.password = password;
     }
 
-    public User(int id, String userName, String password, String firstName, String lastName, List<Blog> blogList) {
+    public User(int id, String username, String password, String firstName, String lastName, List<Blog> blogList) {
         this.id = id;
-        this.userName = userName;
+        this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -45,11 +45,11 @@ public class User{
     public void setId(int id) {
         this.id = id;
     }
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
     public String getPassword() {
         return password;
